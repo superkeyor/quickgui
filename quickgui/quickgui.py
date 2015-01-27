4cd687467c4ebfb019780e766acd94be30678d88
Jerry Zhu <jerryzhujian9@gmail.com>
2015-01-27 09:13:09 -0600

    update

--- quickgui/quickgui.py
+++ quickgui/quickgui.py
@@ -582,6 +582,10 @@ class Log(wx.Frame):
 
             def write(self, message):
                 self.terminal.write(message)
+                 # hack ipython
+                import re
+                if message.startswith('[0;34mln'):
+                    message = re.sub('^\[0.*\[0m','',message)
                 self.log.write(message)
                 self.log.flush()
 
