842d03adeda4de803910c60e51e6d7842dfa1220
Jerry Zhu <jerryzhujian9@gmail.com>
2015-01-28 12:06:53 -0600

    update

--- quickgui/quickgui.py
+++ quickgui/quickgui.py
@@ -498,7 +498,7 @@ class XPrinter(wx.Frame):
             # time.sleep(0.01)   
     """
     def __init__(self):
-        wx.Frame.__init__(self, None, wx.ID_ANY, "Welcome to this App!", style = wx.STAY_ON_TOP)
+        wx.Frame.__init__(self, None, wx.ID_ANY, "Welcome to this App!")
     
         # GUI stuff
         # Add a panel so it looks the correct on all platforms
@@ -514,6 +514,7 @@ class XPrinter(wx.Frame):
         thePanel.SetSizer(theSizer)
         self.Layout()
         self.Center()
+        self.ToggleWindowStyle(wx.STAY_ON_TOP)
         self.Show()
         self.theBtn.SetDefault()
         self.theBtn.SetFocus()
